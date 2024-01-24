d0e362459c35a03e176d4fc2551e87cc0db1c4f0
hantaray <info@alexander-hantke.com>
2024-01-24 20:55:33 +0100

    implemented add book

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 
@@ -22,7 +22,20 @@ def all_books():
         'books': BOOKS
     })
 
-
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(response_object)
 
 if __name__ == '__main__':
     app.run()
